e29e89de7fad3d3a81daa93acfdabd3831f68552
mzq <mazuqiang@lane.com>
2024-05-21 15:09:34 +0800

    修改xterm版本正确

--- web/src/view/server/ServerXterm.vue
+++ web/src/view/server/ServerXterm.vue
@@ -1,54 +1,125 @@
 <template>
-    <el-dialog :width="$root.DialogLargeWidth" :title="dialogTitle" :visible.sync="dialogVisible" @close="dialogCloseHandler">
+    <el-dialog :width="$root.DialogLargeWidth" :title="dialogTitle" :visible.sync="dialogVisible"
+        @close="dialogCloseHandler">
         <div class="app-dialog" v-loading="dialogLoading">
-            
-            <div id="xtermDialog" ref="terminal" ></div>
-
+            <div id="xtermDialog" ref="terminal"></div>
         </div>
     </el-dialog>
 </template>
 
-
-
 <script>
-import { 
+import { } from "@/api/server";
 
-} from '@/api/server'
-
-import { Terminal } from 'xterm';
-import { FitAddon } from 'xterm-addon-fit';
-// import '@xterm/xterm/css/xterm.css';
-import Vue from 'vue'
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import "xterm/css/xterm.css";
+import Vue from "vue";
 export default {
     data() {
         return {
             dialogVisible: false,
             dialogLoading: false,
             btnLoading: false,
-            dialogTitle:""
-        }
+            dialogTitle: "",
+            command: ""
+        };
     },
     methods: {
         openXtermDialogHandler() {
-            this.dialogVisible = true
-            Vue.nextTick(function () {
-                const term = new Terminal();
-                const fitAddon = new FitAddon();
-                term.loadAddon(fitAddon);
-                term.open(this.$refs.terminal);
-                fitAddon.fit();
-            }.bind(this))
+            this.dialogVisible = true;
+            Vue.nextTick(
+                function () {
+                    if (this.term) {
+                        return;
+                    }
+                    const term = new Terminal();
+                    // console.log(term.prompt)
+                    term.prompt = () => {
+                        term.write("\r\n\x1b[33m$\x1b[0m ");
+                    };
+                    term.prompt();
+                    term.open(this.$refs.terminal);
+                    const fitAddon = new FitAddon();
+                    term.loadAddon(fitAddon);
+                    fitAddon.fit()
+
+                    this.term = term;
+                    this.runFakeTerminal();
+
+
+                    // 内容全屏显示-窗口大小发生改变时
+                    function resizeScreen(size) {
+                        console.log("size", size);
+                        try {
+                            fitAddon.fit();
+
+                            // 窗口大小改变时触发xterm的resize方法，向后端发送行列数，格式由后端决定
+                            term.onResize(size => {
+                                // _this.onSend({ Op: "resize", Cols: size.cols, Rows: size.rows });
+                            });
+                        } catch (e) {
+                            console.log("e", e.message);
+                        }
+                    }
+                    window.addEventListener("resize", resizeScreen);
+                }.bind(this)
+            );
         },
-        
+
         dialogCloseHandler() {
-            this.dialogVisible = false
-            this.dialogLoading = false
-            this.btnLoading = false
+            this.dialogVisible = false;
+            this.dialogLoading = false;
+            this.btnLoading = false;
         },
-    
+
+        runFakeTerminal() {
+            let _this = this;
+            let term = _this.term;
+            if (term._initialized) return;
+            term._initialized = true;
+            term.writeln("Welcome to \x1b[1;32m麦考林\x1b[0m.");
+            term.writeln(
+                "This is Web Terminal of Modb; Good Good Study, Day Day Up."
+            );
+            term.prompt();
+            // 添加事件监听器，支持输入方法
+            term.onKey((e) => {
+                const printable =
+                    !e.domEvent.altKey &&
+                    !e.domEvent.altGraphKey &&
+                    !e.domEvent.ctrlKey &&
+                    !e.domEvent.metaKey;
+                if (e.domEvent.keyCode === 13) {
+                    term.prompt();
+                    console.log("回车，发送命令", _this.command)
+                    _this.command = "";
+                } else if (e.domEvent.keyCode === 8) {
+                    // back 删除的情况
+                    if (term._core.buffer.x > 2) {
+                        term.write("\b \b");
+                        _this.command = _this.command.slice(0, -1)
+                    }
+                } else if (printable) {
+                    term.write(e.key);
+                    _this.command += e.key;
+                }
+            });
+
+            term.onData((key) => {
+                // 粘贴的情况
+                if (key.length > 1) {
+                    term.write(key);
+                    _this.command += key;
+                }
+            });
+
+        },
     },
     mounted() {
-        this.$root.BindEventGlobal("openXtermDialogHandler", this.openXtermDialogHandler);
-    }
-}
+        this.$root.BindEventGlobal(
+            "openXtermDialogHandler",
+            this.openXtermDialogHandler
+        );
+    },
+};
 </script>
